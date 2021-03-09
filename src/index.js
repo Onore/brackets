5ebb3d6f50f482a9c23921e3f1f288dab21d1760
Onore <job.kyiv@gmail.com>
2021-03-09 12:19:18 +0200

    feat: complite function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(s, confArr) {
+
+  const leftArr = [];
+  const confLeft = new Map(confArr);
+  
+  s = s.split('')
+    .some(el => {
+      if ((confLeft.has(el) && confLeft.get(el) != el) ||
+        (confLeft.get(el) == el && leftArr[leftArr.length - 1] !== el)) {
+        leftArr.push(el);
+      } else if (confLeft.get(leftArr[leftArr.length - 1]) === el) {
+        leftArr.pop();
+      } else {
+        leftArr.length = 1;
+        return true;
+      }
+    });
+
+  return leftArr.length === 0 ? true : false;
 }
